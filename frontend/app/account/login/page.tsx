41ef1f6be5270e53c36aaa1dc3a21413b2612fff
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-05-15 21:37:08 +0600

    login page design completed

---
+++ frontend/app/account/login/page.tsx
@@ -0,0 +1,17 @@
+import Login from "@/pages/login";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Login account",
+  description: "FoodiFy your best food menu",
+};
+
+const LoginPage = () => {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+};
+
+export default LoginPage;
--- frontend/pages/home/index.tsx
+++ frontend/pages/home/index.tsx
@@ -1,7 +1,7 @@
 export default function HomeContent() {
   return (
-    <div>
-      <h1 className="text-2xl">Hello Food Lover</h1>
+    <div className="w-full h-screen flex justify-center items-center">
+      <h1 className="text-2xl font-bold">Hello Food Lovers</h1>
     </div>
   );
 }
