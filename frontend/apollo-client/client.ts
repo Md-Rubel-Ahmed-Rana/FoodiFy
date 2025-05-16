33a7fe8713d0ba62197cb3c29794e36d100a3588
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-05-16 19:03:57 +0600

    Google login button implemented

--- frontend/apollo-client/client.ts
+++ frontend/apollo-client/client.ts
@@ -1,8 +1,8 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-const apiUri = process.env.NEXT_PUBLIC_BASE_API as string;
+export const apiUri = process.env.NEXT_PUBLIC_BASE_API as string;
 
 export const client = new ApolloClient({
-  uri: apiUri,
+  uri: `${apiUri}/graphql`,
   cache: new InMemoryCache(),
 });
