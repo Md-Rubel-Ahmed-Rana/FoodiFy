e98cc9512182470835840ac52516698dac445e2f
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-05-16 19:41:46 +0600

    cors enabled

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
@@ -12,6 +13,12 @@ async function bootstrap() {
   app.setGlobalPrefix('api/v1');
   app.use(cookieParser());
   app.use(morgan('dev'));
+  app.enableCors({
+    origin: 'https://foodify-silk.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  });
+
   const port = configService.get('PORT') && Number(configService.get('PORT'));
   await app.listen(port);
 }
